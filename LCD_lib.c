524f22ec24ddf93ef145fe5ec26712221d197d09
risko <ceserik@github.com>
2021-05-18 19:01:27 +0200

    asdasd

---
+++ LCD_lib.c
@@ -0,0 +1,4 @@
+#include "LCD_lib.h"
+#include "stdio.h"
+#include "stdlib.h"
+void WriteChar()
---
+++ LCD_lib.h
@@ -0,0 +1,10 @@
+#ifndef LCD_lib_H
+#define LCD_lib_H
+
+#include <stdint.h>
+
+
+void WriteChar(uint16_t * matrix ,int x, int y,char c);
+
+
+#endif  
