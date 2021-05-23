8d67b82ed08febfc8dee671ac4735db1fddce900
risko <ceserik@github.com>
2021-05-23 20:34:02 +0200

    ppm creation and other debugging tools

--- LCD_lib.h
+++ LCD_lib.h
@@ -4,8 +4,19 @@
 #include <stdint.h>
 
 extern uint16_t S[16];
+extern uint16_t P[16];
+extern uint16_t E[16];
+extern uint16_t D[16];
 extern uint16_t C[16];
 extern uint16_t O[16];
+extern uint16_t Equal[16];
+extern uint16_t I[16];
+extern uint16_t F[16];
+//extern uint16_t O[16];
+//extern uint16_t O[16];
+//extern uint16_t O[16];
+//extern uint16_t O[16];
+
 
 void WriteChar(uint16_t *matrix, int Xoffset, int Yoffset, uint16_t* c, uint16_t color,int size);
 void RefreshLCD(unsigned char* membase, uint16_t * matrix);
--- test.c
+++ test.c
@@ -1,21 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "LCD_lib.h"
+#include "font_types.h"
+
+
+int saveBmp(FILE* file, unsigned short * matrix);
+void WriteDefault(unsigned short * matrix);
+
 
 int main()
 {
+
+    FILE *outfile = fopen("matrix.ppm", "wb");
     unsigned short *matrix = (unsigned short *)malloc(480 * 320 * sizeof(uint16_t));
-    WriteChar(matrix, 10, 0, S, 0x07E0,5);
-    WriteLineHorizon(matrix,1,1,0x07E0, 3,50);
-    WriteLineVert(matrix,1,1,0x07E0,3,50);
+
+    WriteDefault(matrix);
     for (int y = 0; y < 320; y++)
     {
         for (int x = 0; x < 480; x++)
         {
-            printf("%d ", (matrix[(480*y)+x])/2016);
+            //printf("%d ", (matrix[(480*y)+x])/2016);
         }
-        printf("\n");
+        //printf("\n");
     }
-    printf("\n");
+   // printf("\n");
+    saveBmp(outfile,matrix);
+    fclose(outfile);
+    
+
     return 0;
+   
 }
+
+void WriteDefault(unsigned short * matrix){
+    WriteLineHorizon(matrix,0,0,0x07E0, 3,480);
+    WriteLineHorizon(matrix,0,317,0x07E0, 3,480);
+    WriteLineVert(matrix,477,0,0x07E0,3,320);
+    WriteLineVert(matrix,0,0,0x07E0,3,320);
+
+    int size = 3;
+    // SPEED=
+    WriteChar(matrix, 10, 0, S, 0x07E0,size);
+    WriteChar(matrix, 10+(1*(size+1)*8), 0, P, 0x07E0,size);
+    WriteChar(matrix, 10+(2*(size+1)*8), 0, E, 0x07E0,size);
+    WriteChar(matrix, 10+(3*(size+1)*8), 0, E, 0x07E0,size);
+    WriteChar(matrix, 10+(4*(size+1)*8), 0, D, 0x07E0,size);
+    WriteChar(matrix, 10+(6*(size+1)*8), 0, Equal, 0x07E0,size);
+    // SET=
+     WriteChar(matrix, 10+(0*(size+1)*8), (1*(size+1)*16), S, 0x07E0,size);
+     WriteChar(matrix, 10+(0*(size+1)*8), (1*(size+1)*16), S, 0x07E0,size);
+     WriteChar(matrix, 10+(0*(size+1)*8), (1*(size+1)*16), S, 0x07E0,size);
+     WriteChar(matrix, 10+(0*(size+1)*8), (1*(size+1)*16), S, 0x07E0,size);
+    //DIFF
+    WriteChar(matrix, 10 + (0*(size+1)*8), (2*(size+1)*16), D, 0x07E0, size );
+    WriteChar(matrix, 10+(1*(size+1)*8), (2*(size+1)*16), I, 0x07E0, size);
+    WriteChar(matrix, 10+(2*(size+1)*8), (2*(size+1)*16), F, 0x07E0, size);
+    WriteChar(matrix, 10+(3*(size+1)*8), (2*(size+1)*16), F, 0x07E0, size);
+    WriteChar(matrix, 10+(4*(size+1)*8), (2*(size+1)*16), Equal, 0x07E0, size);
+
+
+    // P=
+    WriteChar(matrix, 10,(3*(size+1)*16),P,0x07E0,size );
+    WriteChar(matrix, 10+(1*(size+1)*8), (3*(size+1)*16), Equal, 0x07E0,size);
+    // I=
+    WriteChar(matrix, 10 + (3*(size+1)*8),(3 * (size+1)*16),I,0x07E0,size );
+    WriteChar(matrix, 10+(4*(size+1)*8), (3 * (size+1)*16), Equal, 0x07E0,size);
+    // D=
+    WriteChar(matrix, 10 + (6*(size+1)*8), (3*(size+1)*16), D, 0x07E0, size );
+    WriteChar(matrix, 10+(7*(size+1)*8), (3*(size+1)*16), Equal, 0x07E0, size);
+}
+
+
+int saveBmp(FILE* outfile, unsigned short * matrix){
+
+   fputs("P6\n",outfile);
+   fputs("480\n",outfile);
+   fputs("320\n",outfile);
+   fputs("255\n",outfile);
+    int length = 320*480;
+    for (int i = 0; i < length; i++){
+        
+        uint8_t R = matrix[i]<<11;
+        uint8_t G = matrix[i]<<6;
+        uint8_t B = matrix[i]<<1;
+        fputc(R, outfile);
+        fputc(G,outfile);
+        fputc(B,outfile);
+        //printf("XDDD\n");
+    }
+}
+
+
+
